0221bc180ffe34557a360f12f53ed42294d042b9
Tsuki Makes Games <230247964+TsukiMakesGames@users.noreply.github.com>
2025-09-04 17:23:09 +0200

    Started project

---
+++ main.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string quoteToGuess = "herlo";
+char[] dashedQuote = new string('_', quoteToGuess.Length).ToCharArray();
+
+int life = 5;
+
+while (true)
+{
+    Console.WriteLine(new string(dashedQuote));
+    Console.WriteLine("Insert a letter: ");
+    char letter = Console.ReadKey().KeyChar;
+    bool containsLetter = quoteToGuess.Contains(letter);
+    if (containsLetter)
+    {
+        for (int i = 0; i < quoteToGuess.Length; i++)
+        {
+            if (quoteToGuess[i] == letter)
+            {
+                dashedQuote[i] = quoteToGuess[i];
+            }
+        }
+    }
+    else
+    {
+        life--;
+    }
+
+    if (life == 0)
+    {
+        break;
+    }
+}
